2dfd9d6733bc085a49db16c3ee1a248bdf1f40f9
CalvinFehl <63098365+CalvinFehl@users.noreply.github.com>
2026-05-26 15:14:49 +0200

    Reapply "Animation System (Procedural)"
    This reverts commit 4029857bde6632406cf5ac613c84f62620f011dc.

---
+++ Assets/Features/Player/Animation/PitchYawRollAnim.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PitchYawRollAnim : Animateable
+{
+    [SerializeField] private Vector3 axesMultiplyer;
+
+    public override void Animate(float time)
+    {
+        base.Animate(time);
+        
+        Vector3 rotation = transform.localEulerAngles;
+        rotation.x = current * axesMultiplyer.x;
+        rotation.y = current * axesMultiplyer.y;
+        rotation.z = current * axesMultiplyer.z;
+        transform.localEulerAngles = rotation;
+    }
+}
