57f28211517a309d1085b3c5b49acfeda3983ee0
Calvin <calvinfehl@hotmail.com>
2026-06-05 21:29:38 +0200

    Cheeeeeeeeese

--- Assets/Features/Items/StartRotation.cs
+++ Assets/Features/Items/StartRotation.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 
 public class StartRotation : MonoBehaviour
 {
-    public bool rotateX, rotateY, rotateZ;
-    [SerializeField] private float rotationInterval = 90f;
+    [SerializeField] private Vector3 startRotation;
+    public bool randomRotateX, randomRotateY, randomRotateZ;
+    [SerializeField] private float randomRotationInterval = 90f;
 
     void OnEnable()
     {
-        int range = Mathf.RoundToInt(360f / rotationInterval);
+        transform.rotation = Quaternion.Euler(startRotation);
+
+        if (randomRotationInterval == 0f)
+            return;
+
+        int range = Mathf.RoundToInt(360f / randomRotationInterval);
         int randomXRotation = Random.Range(0, range) * 90;
         int randomYRotation = Random.Range(0, range) * 90;
         int randomZRotation = Random.Range(0, range) * 90;
 
-        float rotX = rotateX ? randomXRotation : 0;
-        float rotY = rotateY ? randomYRotation : 0;
-        float rotZ = rotateZ ? randomZRotation : 0;
+        float rotX = randomRotateX ? randomXRotation : 0;
+        float rotY = randomRotateY ? randomYRotation : 0;
+        float rotZ = randomRotateZ ? randomZRotation : 0;
 
         transform.Rotate(rotX, rotY, rotZ);
     }
