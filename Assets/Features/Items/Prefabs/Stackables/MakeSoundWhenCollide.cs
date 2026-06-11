1b550dbc463e80c71c5d4d7dcf5fc530f5d0ebf4
Calvin <calvinfehl@hotmail.com>
2026-06-11 11:03:28 +0200

    Cargo makes a sound when colliding now

---
+++ Assets/Features/Items/Prefabs/Stackables/MakeSoundWhenCollide.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using FMODUnity;
+
+public class MakeSoundWhenCollide : MonoBehaviour
+{
+    [SerializeField] private EventReference soundEvent;
+    [SerializeField] private float minCollisionForce = 1f, coolDown = 0.2f;
+
+    private float lastSoundTime;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision == null) return;
+        
+        float force = collision.relativeVelocity.magnitude;
+        float timeSinceLastSound = Time.time - lastSoundTime;
+        
+        if (force >= minCollisionForce && timeSinceLastSound >= coolDown)
+        {
+            RuntimeManager.PlayOneShot(soundEvent, transform.position);
+            lastSoundTime = Time.time;
+        }
+    }
+}
